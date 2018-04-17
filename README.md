837dc67e930d7b7c06efbe93886ec7ff9c418eeb
Juan Alcocer <juanalcocer95@gmail.com>
2018-04-17 23:20:21 +0200

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+<pre>
+
+Project .....: JavaAppSwingManualTemplate
+Summary .....: Template for Java Application Projects on NetBeans IDE
+Created .....: 16/04/2018
+Updated .....: 16/04/2018
+Author ......: Juan Alcocer - juanalcocer95@gmail.com
+Platform ....: Java 8
+IDE .........: NetBeans 8.2
+Remote ......: https://github.com/juanalcocer95/JavaAppSwingManualTemplate
+
+---
+
+Please, clone the right branch according with your Application type. 
+
+</pre>
---
+++ src/org/japo/java/forms/GUI.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2017 José A. Pacheco Ondoño - joanpaon@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.forms;
+
+import java.util.Properties;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.japo.java.libraries.UtilesSwing;
+
+/**
+ *
+ * @author Juan Alcocer Canet - juanasir1995@gmail.com
+ */
+public class GUI extends JFrame {
+
+    // Propiedades App
+    public static final String PRP_LOOK_AND_FEEL = "look_and_feel";
+    public static final String PRP_FAVICON = "favicon";
+
+    // Valores por Defecto
+    public static final String DEF_LOOK_AND_FEEL = UtilesSwing.LNF_NIMBUS;
+    public static final String DEF_FAVICON = "img/favicon.png";
+
+    // Referencias
+    private Properties prp;
+
+    // Constructor
+    public GUI(Properties prp) {
+        // Inicialización Anterior
+        initBefore(prp);
+
+        // Creación Interfaz
+        initComponents();
+
+        // Inicializacion Posterior
+        initAfter();
+    }
+
+    // Construcción - GUI
+    private void initComponents() {
+        // Panel Principal
+        JPanel pnlPpal = new JPanel();
+
+        // Ventana Principal
+        setContentPane(pnlPpal);
+        setTitle("Swing Manual #00");
+        setResizable(false);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    // Inicialización Anterior    
+    private void initBefore(Properties prp) {
+        // Memorizar Referencia
+        this.prp = prp;
+
+        // Establecer LnF
+        UtilesSwing.establecerLnF(prp.getProperty(PRP_LOOK_AND_FEEL, DEF_LOOK_AND_FEEL));
+    }
+
+    // Inicialización Posterior
+    private void initAfter() {
+        // Establecer Favicon
+        UtilesSwing.establecerFavicon(this, prp.getProperty(PRP_FAVICON, DEF_FAVICON));
+    }
+}
